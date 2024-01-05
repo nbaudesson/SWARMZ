668af490f7c04afeaf24c9e6374de222d13a111f
nb_adm <nicolas.baudesson@alten.com>
2024-01-05 17:16:44 +0100

    added launcher for acoustic scenarios

--- SwarmZ_ROS2/src/acoustics_simulation_py/setup.py
+++ SwarmZ_ROS2/src/acoustics_simulation_py/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
