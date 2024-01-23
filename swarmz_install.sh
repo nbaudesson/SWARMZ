da9a162837f023b74d6e870c3ed9658eef831611
nb_adm <nicolas.baudesson@alten.com>
2024-01-23 16:44:21 +0100

    numpy==1.24

--- swarmz_install.sh
+++ swarmz_install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 sudo apt install -y build-essential
-# You need at least python 3.10 for numpy dependencies
-desired_python_version="3.10"
-# Get the installed Python version
-installed_python_version=$(python3 --version 2>&1 | awk '{print $2}' | cut -d'.' -f1,2)
-# Compare the versions
-if [[ "$(printf "%s\n" "$desired_python_version" "$installed_python_version" | sort -V | head -n 1)" == "$desired_python_version" ]]; then
-    echo "Python $desired_python_version or above is already installed."
-else
-    echo "Installing Python 3.11..."
-    sudo apt update
-    sudo apt install software-properties-common -y
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt update
-    sudo apt install python3.11 -y
-    # ln -s /usr/bin/python3.11 /usr/bin/python
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$installed_python_version 2
-    sudo update-alternatives --config python3
-    sudo apt install python-is-python3 -y
-    sudo apt-get install curl -y
-    sudo apt-get install -y python3-testresources python3-launchpadlib
-    curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
-fi
+# # You need at least python 3.10 for numpy dependencies
+# desired_python_version="3.10"
+# # Get the installed Python version
+# installed_python_version=$(python3 --version 2>&1 | awk '{print $2}' | cut -d'.' -f1,2)
+# # Compare the versions
+# if [[ "$(printf "%s\n" "$desired_python_version" "$installed_python_version" | sort -V | head -n 1)" == "$desired_python_version" ]]; then
+#     echo "Python $desired_python_version or above is already installed."
+# else
+#     echo "Installing Python 3.11..."
+#     sudo apt update
+#     sudo apt install software-properties-common -y
+#     sudo add-apt-repository ppa:deadsnakes/ppa
+#     sudo apt update
+#     sudo apt install python3.11 -y
+#     # ln -s /usr/bin/python3.11 /usr/bin/python
+#     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+#     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$installed_python_version 2
+#     sudo update-alternatives --config python3
+#     sudo apt install python-is-python3 -y
+#     sudo apt-get install curl -y
+#     sudo apt-get install -y python3-testresources python3-launchpadlib
+#     curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
+# fi
 # Check if the system is running Ubuntu
 if [ -f /etc/os-release ]; then
     # Source the os-release file
@@ -51,57 +51,30 @@ if [ -f /etc/os-release ]; then
 
         else
             echo "ROS installation directory not found for the specified Ubuntu version."
-            if [ "$VERSION_ID" = "20.04" ] && [ ! -d "/opt/ros/foxy" ]; then
-                echo "Installing ROS Foxy"
-                ros_distro="foxy"
-                sudo apt update -y && sudo apt install locales -y
-                sudo locale-gen en_US en_US.UTF-8
-                sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-                export LANG=en_US.UTF-8
-                sudo apt install software-properties-common -y
-                sudo add-apt-repository universe
-                sudo apt update -y && sudo apt install curl -y
-                sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" |
-                sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-                sudo apt update -y && sudo apt upgrade -y
-                sudo apt install ros-foxy-desktop -y
-                sudo apt install ros-dev-tools -y
-                source /opt/ros/foxy/setup.bash && echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+            echo "Installing ROS Foxy"
+            ros_distro="foxy"
+            sudo apt update -y && sudo apt install locales -y
+            sudo locale-gen en_US en_US.UTF-8
+            sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+            export LANG=en_US.UTF-8
+            sudo apt install software-properties-common -y
+            sudo add-apt-repository universe
+            sudo apt update -y && sudo apt install curl -y
+            sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+            echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" |
+            sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+            sudo apt update -y && sudo apt upgrade -y
+            sudo apt install ros-$ros_distro-desktop -y
+            sudo apt install ros-dev-tools -y
+            source /opt/ros/$ros_distro/setup.bash && echo "source /opt/ros/$ros_distro/setup.bash" >> ~/.bashrc
 
-                sudo apt-get install -y python3-testresources python-launchpadlib
-                sudo apt install python3-pip -y
-                pip install --user -U pyros-genmsg
-                pip install --user setuptools==58.2.0
-                pip install empy==3.3.4
-                pip install -U colcon-common-extensions
-
-            elif [ "$VERSION_ID" = "22.04" ] && [ ! -d "/opt/ros/humble" ]; then
-                echo "Installing ROS Humble"
-                ros_distro="humble"
-                sudo apt update -y && sudo apt install locales -y
-                sudo locale-gen en_US en_US.UTF-8
-                sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-                export LANG=en_US.UTF-8
-                sudo apt install software-properties-common -y
-                sudo add-apt-repository universe
-                sudo apt update -y && sudo apt install curl -y
-                sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" |
-                sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-                sudo apt update -y && sudo apt upgrade -y
-                sudo apt install ros-humble-desktop -y
-                sudo apt install ros-dev-tools -y
-                source /opt/ros/humble/setup.bash && echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-
-                sudo apt-get install -y python3-testresources python-launchpadlib
-                sudo apt install python3-pip -y
-                pip install --user -U pyros-genmsg
-                pip install --user setuptools==58.2.0
-                pip install empy==3.3.4
-                pip install -U colcon-common-extensions
-
-            fi
+            sudo apt-get install -y python3-testresources python3-launchpadlib
+            sudo apt install python3-pip -y
+            pip install --user -U pyros-genmsg
+            pip install --user setuptools==58.2.0
+            pip install empy==3.3.4
+            pip install numpy==1.24
+            pip install -U colcon-common-extensions
         fi
     else
         echo "This script is designed for Ubuntu 20.04 or 22.04, but the system does not match the criteria."
@@ -132,23 +105,6 @@ sudo apt install ros-$ros_distro-actuator-msgs -y
 ### ros-$ros_distro-plotjuggler-ros ###
 sudo apt install ros-$ros_distro-plotjuggler-ros -y
 
-### Gazebo garden (harmonic didn't work (yet)) ###
-sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
-sudo apt-get update -y
-sudo apt remove gz-classic -y ; sudo apt remove gz-garden -y ; sudo apt remove gz-harmonic -y ; sudo apt autoremove -y
-# sudo apt-get install gz-garden -y
-### Apparently it works now ?
-if [ "$ros_distro" = "foxy" ]; then
-echo "ros_distro" = "$ros_distro"
-sudo apt-get install gz-garden -y
-fi
-if [ "$ros_distro" = "humble" ]; then
-echo "ros_distro" = "$ros_distro"
-sudo apt-get install gz-harmonic -y
-fi
-# sudo apt remove gz-harmonic -y ; sudo apt autoremove -y
-
 ### gflags (from binary) ###
 sudo apt-get install libgflags-dev -y
 
@@ -172,6 +128,24 @@ make distclean
 make px4_sitl
 
 cd ..
+
+
+### Gazebo garden (harmonic didn't work (yet)) ###
+sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update -y
+# sudo apt remove gz-classic -y ; sudo apt remove gz-garden -y ; sudo apt remove gz-harmonic -y ; sudo apt autoremove -y
+# sudo apt-get install gz-garden -y
+### Apparently it works now ?
+if [ "$ros_distro" = "foxy" ]; then
+echo "ros_distro" = "$ros_distro"
+sudo apt-get install gz-garden -y
+fi
+if [ "$ros_distro" = "humble" ]; then
+echo "ros_distro" = "$ros_distro"
+sudo apt-get install gz-harmonic -y
+fi
+# sudo apt remove gz-harmonic -y ; sudo apt autoremove -y
 
 ### custom map ### 124x72m
 cp map/swarmzmap.sdf PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf
